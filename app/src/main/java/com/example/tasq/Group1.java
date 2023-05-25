cc1f76e0e2695b7e5243a6a76399b99b9e62163e
AllenFuncion <vafuncion@gmail.com>
2023-05-25 15:16:31 +0800

    High Priority Task

--- app/src/main/java/com/example/tasq/Group1.java
+++ app/src/main/java/com/example/tasq/Group1.java
@@ -2,7 +2,9 @@ package com.example.tasq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Group1 extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class Group1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
         getSupportActionBar().hide();
+    }
+    public void goBackToHome(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/tasq/Group2.java
+++ app/src/main/java/com/example/tasq/Group2.java
@@ -2,7 +2,9 @@ package com.example.tasq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Group2 extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class Group2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group2);
         getSupportActionBar().hide();
+    }
+    public void goBackToHome(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        finish();
     }
 }
