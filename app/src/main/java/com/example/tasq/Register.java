5f867982df2619045d84953fb89d864f775a8a54
AllenFuncion <vafuncion@gmail.com>
2023-05-29 03:00:18 +0800

    Register Level Reset

--- app/src/main/java/com/example/tasq/Register.java
+++ app/src/main/java/com/example/tasq/Register.java
@@ -2,6 +2,7 @@ package com.example.tasq;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,6 +45,12 @@ public class Register extends AppCompatActivity {
                             Boolean insert = DB.insertData(user, pass);
                             if(insert==true){
                                 Toast.makeText(Register.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+
+                                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putInt("currentLevel", 1);
+                                editor.apply();
+
                                 Intent intent = new Intent(getApplicationContext(),Home.class);
                                 startActivity(intent);
                             }else{
